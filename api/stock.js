b19fb9e90991d9f5a0bfd57cc6168b48ebf0f351
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-05-13 17:03:16 +0530

    Ok

---
+++ api/stock.js
@@ -0,0 +1,14 @@
+module.exports = async (req, res) => {
+    try {
+        const { ticker } = req.query;
+        const data = await rest.stocks.aggregates(ticker, 1, 'day', '2024-01-01', '2025-05-11');
+
+        if (!data.results) {
+            return res.status(404).json({ error: 'No stock data found' });
+        }
+        res.json(data);
+    } catch (e) {
+        console.error('Error fetching stock data:', e);
+        res.status(500).json({ error: 'Server Error: ' + e.message });
+    }
+};
